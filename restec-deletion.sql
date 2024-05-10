2146991c8e31f1196a36bf9cbad1610713a1f71b
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-05-09 22:55:25 -0600

    Update restec-deletion.sql

--- restec-deletion.sql
+++ restec-deletion.sql
@@ -1,4 +1,5 @@
 ALTER TABLE ScheduleSlots DROP CONSTRAINT BookingUser;
 ALTER TABLE ScheduleSlots DROP CONSTRAINT IsBookedDefault;
+ALTER TABLE ScheduleSlots DROP CONSTRAINT PeopleQuantityDefault;
 DROP TABLE UserData;
 DROP TABLE ScheduleSlots;
