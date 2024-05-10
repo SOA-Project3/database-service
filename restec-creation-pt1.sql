e052c09193dc811337bdd3901c82e27098fc6d09
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-05-09 22:53:11 -0600

    Update restec-creation-pt1.sql

--- restec-creation-pt1.sql
+++ restec-creation-pt1.sql
@@ -1,27 +1,24 @@
 -- User
 CREATE TABLE UserData
 (
-	Id INT NOT NULL,
-	Fullname VARCHAR(50) NOT NULL,
-	Rol VARCHAR(20),
-	Password VARCHAR(100) NOT NULL,
-
-	PRIMARY KEY (Id)
+    UserId VARCHAR(255) NOT NULL,
+    Fullname VARCHAR(50) NOT NULL,
+    Rol VARCHAR(20),
+    Password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (UserId)
 );
 
 -- ScheduleSlots
 CREATE TABLE ScheduleSlots
 (
-	Id INT IDENTITY(1,1) NOT NULL,
-	Date DATE NOT NULL,
-	Time TIME NOT NULL,
-	IsBooked VARCHAR(5) NOT NULL,
-	PeopleQuantity INT,
-    UserId INT,
-
-	PRIMARY KEY (Id)
+    Id INT IDENTITY(1,1) NOT NULL,
+    Date DATE NOT NULL,
+    Time TIME NOT NULL,
+    IsBooked VARCHAR(5) NOT NULL DEFAULT 'NO',
+    PeopleQuantity INT DEFAULT 0,
+    UserId VARCHAR(255), -- Change UserId to VARCHAR
+    PRIMARY KEY (Id)
 );
-
 -- Default values
 
 ALTER TABLE ScheduleSlots
