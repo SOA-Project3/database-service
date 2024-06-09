5d02501f82378a250c465660de700058ce1859ab
Valesska Blanco <79022669+valesskablanco@users.noreply.github.com>
2024-06-09 16:22:40 -0600

    Update restec-population.sql

--- restec-population.sql
+++ restec-population.sql
@@ -1,73 +1,150 @@
--- Crear usuarios de ejemplo
+-- Crear usuarios iniciales
 INSERT INTO UserData (Id, Fullname, Rol, Password)
 VALUES
     ('luismorarod98@gmail.com', 'Luis Pedro Morales', 'Admin', HASHBYTES('SHA2_256', 'password123')),
     ('geogd.712@gmail.com', 'Geovanny García', 'User', HASHBYTES('SHA2_256', 'password123')),
-    ('valesskitab@gmail.com', 'Valesska Blanco', 'User', HASHBYTES('SHA2_256', 'password123')),
-    ('academicodiana08@gmail.com', 'Diana Mejías', 'Admin', HASHBYTES('SHA2_256', 'password123'));
+    ('valesskitab@gmail.com', 'Valesska Blanco', 'Admin', HASHBYTES('SHA2_256', 'password123')),
+    ('academicodiana08@gmail.com', 'Diana Mejías', 'Admin', HASHBYTES('SHA2_256', 'password123')),
+    ('harrystyles@gmail.com', 'Harry Styles', 'SuperAdmin', HASHBYTES('SHA2_256', 'password123'));
 
+-- Crear sucursales disponibles
+INSERT INTO Branch (Id, Admin)
+VALUES
+    ('New York', 'valesskitab@gmail.com'),
+    ('Paris', 'academicodiana08@gmail.com'),
+    ('Costa Rica', 'luismorarod98@gmail.com');
 
--- Crear espacios de horario para una semana completa (del 1 al 7 de mayo de 2024)
+-- Crear espacios de horario para una semana completa
 -- Horarios de 12pm a 10pm, cada 2 horas
--- Lunes (1 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Lunes
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-01 12:00:00', 'No'),
-    ('2024-05-01 14:00:00', 'No'),
-    ('2024-05-01 16:00:00', 'No'),
-    ('2024-05-01 18:00:00', 'No'),
-    ('2024-05-01 20:00:00', 'No');
+    ('New York', '2024-06-10 12:00:00', 'No'),
+    ('New York', '2024-06-10 14:00:00', 'No'),
+    ('New York', '2024-06-10 16:00:00', 'No'),
+    ('New York', '2024-06-10 18:00:00', 'No'),
+    ('New York', '2024-06-10 20:00:00', 'No'),
+    ('Paris', '2024-06-10 12:00:00', 'No'),
+    ('Paris', '2024-06-10 14:00:00', 'No'),
+    ('Paris', '2024-06-10 16:00:00', 'No'),
+    ('Paris', '2024-06-10 18:00:00', 'No'),
+    ('Paris', '2024-06-10 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-10 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-10 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-10 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-10 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-10 20:00:00', 'No');
 
--- Martes (2 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Martes
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-02 12:00:00', 'No'),
-    ('2024-05-02 14:00:00', 'No'),
-    ('2024-05-02 16:00:00', 'No'),
-    ('2024-05-02 18:00:00', 'No'),
-    ('2024-05-02 20:00:00', 'No');
+    ('New York', '2024-06-11 12:00:00', 'No'),
+    ('New York', '2024-06-11 14:00:00', 'No'),
+    ('New York', '2024-06-11 16:00:00', 'No'),
+    ('New York', '2024-06-11 18:00:00', 'No'),
+    ('New York', '2024-06-11 20:00:00', 'No'),
+    ('Paris', '2024-06-11 12:00:00', 'No'),
+    ('Paris', '2024-06-11 14:00:00', 'No'),
+    ('Paris', '2024-06-11 16:00:00', 'No'),
+    ('Paris', '2024-06-11 18:00:00', 'No'),
+    ('Paris', '2024-06-11 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-11 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-11 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-11 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-11 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-11 20:00:00', 'No');
 
--- Miércoles (3 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Miércoles
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-03 12:00:00', 'No'),
-    ('2024-05-03 14:00:00', 'No'),
-    ('2024-05-03 16:00:00', 'No'),
-    ('2024-05-03 18:00:00', 'No'),
-    ('2024-05-03 20:00:00', 'No');
+    ('New York', '2024-06-12 12:00:00', 'No'),
+    ('New York', '2024-06-12 14:00:00', 'No'),
+    ('New York', '2024-06-12 16:00:00', 'No'),
+    ('New York', '2024-06-12 18:00:00', 'No'),
+    ('New York', '2024-06-12 20:00:00', 'No'),
+    ('Paris', '2024-06-12 12:00:00', 'No'),
+    ('Paris', '2024-06-12 14:00:00', 'No'),
+    ('Paris', '2024-06-12 16:00:00', 'No'),
+    ('Paris', '2024-06-12 18:00:00', 'No'),
+    ('Paris', '2024-06-12 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-12 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-12 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-12 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-12 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-12 20:00:00', 'No');
 
--- Jueves (4 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Jueves
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-04 12:00:00', 'No'),
-    ('2024-05-04 14:00:00', 'No'),
-    ('2024-05-04 16:00:00', 'No'),
-    ('2024-05-04 18:00:00', 'No'),
-    ('2024-05-04 20:00:00', 'No');
+    ('New York', '2024-06-13 12:00:00', 'No'),
+    ('New York', '2024-06-13 14:00:00', 'No'),
+    ('New York', '2024-06-13 16:00:00', 'No'),
+    ('New York', '2024-06-13 18:00:00', 'No'),
+    ('New York', '2024-06-13 20:00:00', 'No'),
+    ('Paris', '2024-06-13 12:00:00', 'No'),
+    ('Paris', '2024-06-13 14:00:00', 'No'),
+    ('Paris', '2024-06-13 16:00:00', 'No'),
+    ('Paris', '2024-06-13 18:00:00', 'No'),
+    ('Paris', '2024-06-13 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-13 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-13 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-13 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-13 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-13 20:00:00', 'No');
 
--- Viernes (5 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Viernes
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-05 12:00:00', 'No'),
-    ('2024-05-05 14:00:00', 'No'),
-    ('2024-05-05 16:00:00', 'No'),
-    ('2024-05-05 18:00:00', 'No'),
-    ('2024-05-05 20:00:00', 'No');
+    ('New York', '2024-06-14 12:00:00', 'No'),
+    ('New York', '2024-06-14 14:00:00', 'No'),
+    ('New York', '2024-06-14 16:00:00', 'No'),
+    ('New York', '2024-06-14 18:00:00', 'No'),
+    ('New York', '2024-06-14 20:00:00', 'No'),
+    ('Paris', '2024-06-14 12:00:00', 'No'),
+    ('Paris', '2024-06-14 14:00:00', 'No'),
+    ('Paris', '2024-06-14 16:00:00', 'No'),
+    ('Paris', '2024-06-14 18:00:00', 'No'),
+    ('Paris', '2024-06-14 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-14 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-14 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-14 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-14 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-14 20:00:00', 'No');
 
--- Sábado (6 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Sábado
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-06 12:00:00', 'No'),
-    ('2024-05-06 14:00:00', 'No'),
-    ('2024-05-06 16:00:00', 'No'),
-    ('2024-05-06 18:00:00', 'No'),
-    ('2024-05-06 20:00:00', 'No');
+    ('New York', '2024-06-15 12:00:00', 'No'),
+    ('New York', '2024-06-15 14:00:00', 'No'),
+    ('New York', '2024-06-15 16:00:00', 'No'),
+    ('New York', '2024-06-15 18:00:00', 'No'),
+    ('New York', '2024-06-15 20:00:00', 'No'),
+    ('Paris', '2024-06-15 12:00:00', 'No'),
+    ('Paris', '2024-06-15 14:00:00', 'No'),
+    ('Paris', '2024-06-15 16:00:00', 'No'),
+    ('Paris', '2024-06-15 18:00:00', 'No'),
+    ('Paris', '2024-06-15 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-15 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-15 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-15 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-15 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-15 20:00:00', 'No');
 
--- Domingo (7 de mayo)
-INSERT INTO ScheduleSlots (DateTime, IsBooked)
+-- Domingo
+INSERT INTO ScheduleSlots (Branch, DateTime, IsBooked)
 VALUES
-    ('2024-05-07 12:00:00', 'No'),
-    ('2024-05-07 14:00:00', 'No'),
-    ('2024-05-07 16:00:00', 'No'),
-    ('2024-05-07 18:00:00', 'No'),
-    ('2024-05-07 20:00:00', 'No');
+    ('New York', '2024-06-16 12:00:00', 'No'),
+    ('New York', '2024-06-16 14:00:00', 'No'),
+    ('New York', '2024-06-16 16:00:00', 'No'),
+    ('New York', '2024-06-16 18:00:00', 'No'),
+    ('New York', '2024-06-16 20:00:00', 'No'),
+    ('Paris', '2024-06-16 12:00:00', 'No'),
+    ('Paris', '2024-06-16 14:00:00', 'No'),
+    ('Paris', '2024-06-16 16:00:00', 'No'),
+    ('Paris', '2024-06-16 18:00:00', 'No'),
+    ('Paris', '2024-06-16 20:00:00', 'No'),
+    ('Costa Rica', '2024-06-16 12:00:00', 'No'),
+    ('Costa Rica', '2024-06-16 14:00:00', 'No'),
+    ('Costa Rica', '2024-06-16 16:00:00', 'No'),
+    ('Costa Rica', '2024-06-16 18:00:00', 'No'),
+    ('Costa Rica', '2024-06-16 20:00:00', 'No');
