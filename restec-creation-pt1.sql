cbc390c4e55586a9c63bd0a8433100cfc30c1e4e
Valesska Blanco <79022669+valesskablanco@users.noreply.github.com>
2024-06-09 16:21:36 -0600

    Update restec-creation-pt1.sql

--- restec-creation-pt1.sql
+++ restec-creation-pt1.sql
@@ -8,13 +8,21 @@ CREATE TABLE UserData
     PRIMARY KEY (Id)
 );
 
+CREATE TABLE Branch
+(
+    Id VARCHAR(100) NOT NULL,
+    Admin VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Id)
+);
+
 -- ScheduleSlots
 CREATE TABLE ScheduleSlots
 (
     Id INT IDENTITY(1,1) NOT NULL,
+    Branch VARCHAR(100) NOT NULL,
     DateTime DATETIME NOT NULL,
     IsBooked VARCHAR(5) NOT NULL DEFAULT 'NO',
     PeopleQuantity INT DEFAULT 0,
-    UserId VARCHAR(255), -- Change UserId to VARCHAR
+    UserId VARCHAR(255), 
     PRIMARY KEY (Id)
 );
